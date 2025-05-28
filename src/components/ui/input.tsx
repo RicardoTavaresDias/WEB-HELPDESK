f82aee231d65defa3b3e121faf6e8f3a77f0b4e1
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-05-27 22:44:45 -0300

    version 1.1.5

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,3 +1,5 @@
+import circleAlert from "../../assets/icon/circle-alert.svg"
+
 type Props = {
   type: string
   placeholder: string
@@ -5,14 +7,23 @@ type Props = {
   error?: string
   onChange: any
   value: string
+  textLabel?: string
 }
 
-export function Input({ type, placeholder, label, error, onChange, value}: Props){
+export function Input({ type, placeholder, label, error, onChange, value, textLabel}: Props){
   return (
     <div className="group flex flex-col mt-4">
-      <label className={" text-gray-300 Text-Xxs group-focus-within:text-blue-base"}>{label}</label>
+      <label className={`Text-Xxs group-focus-within:text-blue-base ${error ? "text-feedback-danger" : "text-gray-300"}`}>{label}</label>
       <input type={type} className="border-b-1 border-gray-500 w-86 max-sm:w-73 Heading-Md my-2 pb-2 outline-none group-focus-within:border-blue-base" placeholder={placeholder}  onChange={onChange} value={value} />
-      <span className="Text-Xs text-gray-400" >{error}</span>
+      {error &&
+        <div className="flex gap-1">
+          <img src={circleAlert} className="w-4 h-4"/>
+          <span className="Text-Xs text-feedback-danger" >{error}</span>
+        </div>
+      } 
+      {label &&
+        <span className="Text-Xs text-gray-400" >{textLabel}</span>
+      }
     </div>
   )
 }
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -25,10 +25,10 @@ export function Signup(){
       <div className="bg-gray-600 w-170 lg:h-screen ml-auto px-35 py-12 max-sm:py-8 rounded-tl-3xl max-sm:w-95 max-sm:px-4 max-sm:rounded-3xl max-sm:mx-auto">
         <LogoAuth />
         <form onSubmit={handleSubmit}>
-          <FormAuth textTitle="Crie sua conta" textSpan="Informe seu nome, e-mail e senha" label="Cadastrar">
-            <Input type= "text" label="NOME" placeholder="Digite o nome completo" value={name} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}/>
+          <FormAuth textTitle="Crie sua conta" textSpan="Informe seu nome, e-mail e senha" label="Cadastrar" >
+            <Input type= "text" label="NOME" placeholder="Digite o nome completo" value={name} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)} />
             <Input type= "text" label="E-MAIL" placeholder="exemplo@mail.com" value={email} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
-            <Input type= "password" label="SENHA" placeholder="Digite sua senha" error="Mínimo de 6 dígitos" value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
+            <Input type= "password" label="SENHA" placeholder="Digite sua senha" textLabel="Mínimo de 6 dígitos" value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
           </FormAuth>
         </form>
         <Account h3="Já uma conta?" span="Entre agora mesmo" label="Acessar conta"/>
