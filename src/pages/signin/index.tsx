71cd33aed08b93a63d744b44d4f9e0c40dcc47d4
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-05-29 23:48:44 -0300

    version 1.2.18 - anime Auth

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -21,14 +21,18 @@ export function Signin(){
   return (
     <main className="pt-3 max-sm:pt-8">
       <div className="bg-gray-600 w-170 lg:h-screen ml-auto px-35 py-12 max-sm:py-8 rounded-tl-3xl max-sm:w-95 max-sm:px-4 max-sm:rounded-3xl max-sm:mx-auto">
-        <LogoAuth />
-        <form onSubmit={handleSubmit}>
-          <FormAuth textTitle="Acesse o portal" textSpan="Entre usando seu e-mail e senha cadastrados" label="Entrar">
-            <Input type= "text" label="E-MAIL" placeholder="exemplo@mail.com" value={email} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
-            <Input type= "password" label="SENHA" placeholder="Digite sua senha" value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
-          </FormAuth>
-        </form>
-        <Account h3="Ainda não tem uma conta?" span="Cadastre agora mesmo" label="Criar conta" path="/signup"/>
+        
+        <div id="animeAuth">
+          <LogoAuth />
+          <form onSubmit={handleSubmit}>
+            <FormAuth textTitle="Acesse o portal" textSpan="Entre usando seu e-mail e senha cadastrados" label="Entrar">
+              <Input type= "text" label="e-mail" placeholder="exemplo@mail.com" value={email} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
+              <Input type= "password" label="senha" placeholder="Digite sua senha" value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
+            </FormAuth>
+          </form>
+          <Account h3="Ainda não tem uma conta?" span="Cadastre agora mesmo" label="Criar conta" path="/signup"/>
+        </div>
+
       </div>
     </main>
   )
--- src/routers/admin.tsx
+++ src/routers/admin.tsx
@@ -5,7 +5,7 @@ import { CallList } from "../pages/admin/callList"
 export function Admin(){
   return (
     <Routes>
-      <Route path='/' element={<Layout identification="admin"/>} >
+      <Route element={<Layout identification="admin"/>} >
         <Route path="/chamados" index element={<CallList />} />
         <Route path="/tecnicos" element={"Em Breve..."} />
         <Route path="/clientes" element={"Em Breve..."} />
