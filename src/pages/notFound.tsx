a3e5c5db7cd142a0dd85b23058dae95d4bfa4fbf
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-06-06 17:52:30 -0300

    version 1.9.45

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -4,11 +4,11 @@ export function NotFound(){
   return (
     <>
       <div className="w-screen h-screen flex flex-col justify-center items-center fixed bg-gray-100 text-gray-500/80">
-        <h1 className="text-[170px]" >Oops!</h1>
-        <span className="uppercase text-2xl" >404 - page not Found</span>
-        <p>A página solicitada não pôde ser encontrada</p>
-        <Link to={"/"} className="mt-10">
-          <span className="bg-gray-500 px-10 py-2 rounded text-gray-200 font-semibold">
+        <h1 className="text-[170px] max-sm:text-[90px]" >Oops!</h1>
+        <span className="uppercase text-2xl max-sm:text-base" >404 - page not Found</span>
+        <p className="max-sm:text-sm">A página solicitada não pôde ser encontrada</p>
+        <Link to={"/chamados"} className="mt-10">
+          <span className="bg-gray-500 px-10 py-2 rounded text-gray-200 font-semibold ">
             Voltar
           </span>
         </Link>
