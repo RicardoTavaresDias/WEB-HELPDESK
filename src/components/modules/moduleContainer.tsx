a227bad8efff237e148a2834f8510466396f6cc3
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-06-12 15:24:02 -0300

    version 1.11.82 - adjustment PanelRoot and MaduleContainer

--- src/components/modules/moduleContainer.tsx
+++ src/components/modules/moduleContainer.tsx
@@ -5,7 +5,7 @@ type ContaineProps =  {
 export function ModuleContainer({children}: ContaineProps){
    return (
     <>
-      <div className="flex flex-wrap items-start w-full justify-start gap-6 mt-6 max-sm:flex-col max-sm:mt-4 max-sm:gap-4">
+      <div className="flex flex-wrap items-start w-full justify-start gap-6 mt-6 max-sm:flex-col max-sm:mt-4 max-sm:gap-4 min-lg:justify-center">
         {children}
       </div>
     </>
--- src/components/table/PanelRoot.tsx
+++ src/components/table/PanelRoot.tsx
@@ -7,7 +7,7 @@ type TableProps = {
 export function PanelRoot({children, mobile, className}: TableProps){
   if(mobile){
      return (
-    <div className="lg:hidden border-2 rounded-2xl border-gray-500">
+    <div className="lg:hidden border-2 rounded-xl border-gray-500">
       <div className={`lg:hidden grid ${className} gap-0 rounded-xl`}>
         {children}
       </div>
@@ -16,7 +16,7 @@ export function PanelRoot({children, mobile, className}: TableProps){
   }
 
   return (
-    <div className="max-sm:hidden border-2 rounded-2xl border-gray-500 max-w-[1280px] m-auto">
+    <div className="max-sm:hidden border-2 rounded-xl border-gray-500 max-w-[1280px] m-auto">
       <div className={`grid ${className} gap-0 rounded-xl`}>
         {children}
       </div>
