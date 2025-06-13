e1f1cee7dc8296cbbec5546d5b3de491451c7ec5
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-06-13 10:50:20 -0300

    version 1.11.86

--- src/layout/menuMobile.tsx
+++ src/layout/menuMobile.tsx
@@ -1,7 +1,6 @@
 import { Menu } from "./menu"
 import { useOpenModal } from "../hooks/useOpenModal"
-import { IconX } from "../assets/icon/iconX"
-
+import { IconMobile } from "../assets/icon/iconMobile"
 
 export function MenuMobile({ element }: any){
   const { open, menuRef, setOpen } = useOpenModal()
@@ -9,7 +8,7 @@ export function MenuMobile({ element }: any){
   return (
     <div ref={menuRef}>
       <button type="button" className="group" onClick={() => setOpen(!open)} >
-        <IconX className="w-10 h-10 cursor-pointer fill-gray-600 bg-gray-200 p-2.5 rounded" isActive={open} />
+        <IconMobile className="w-10 h-10 cursor-pointer fill-gray-600 bg-gray-200 p-2.5 rounded" isActive={open} />
       </button>
 
       <div className={`absolute left-3 top-24 z-10 ${open ? "scale-y-100" : "scale-y-0"} origin-top duration-200 bg-gray-100 flex flex-col gap-3 w-87 rounded-xl p-4`} >
