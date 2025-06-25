ade614d9ec659467ae1f74dd050a232d1508e585
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-06-25 13:06:56 -0300

    version 1.12.104

--- src/lib/formatHours.ts
+++ src/lib/formatHours.ts
@@ -7,27 +7,27 @@ type userHoursReturn = {
 }[]
 
 export const formatHours = (userHours: string[]): userHoursReturn => {
-    let morning: Date[] = []
-    let afternoon: Date[] = []
-    let night: Date[] =  []
+  let morning: Date[] = []
+  let afternoon: Date[] = []
+  let night: Date[] =  []
 
-    for(const hour of userHours){
-      const date = dayjs(`${dayjs().format("YYYY-MM-DD")}${hour}`, "YYYY-MM-DD HH:mm").toDate()
+  for(const hour of userHours){
+    const date = dayjs(`${dayjs().format("YYYY-MM-DD")}${hour}`, "YYYY-MM-DD HH:mm").toDate()
 
-      if(day.morning.includes(hour)){
-        morning.push(date) 
-      } else if(day.afternoon.includes(hour)){
-        afternoon.push(date) 
-      }else {
-        night.push(date) 
-      }
+    if(day.morning.includes(hour)){
+      morning.push(date) 
+    } else if(day.afternoon.includes(hour)){
+      afternoon.push(date) 
+    }else {
+      night.push(date) 
     }
-
-    return (
-        [
-          {startTime: morning.sort()[0] || null, endTime: morning.sort()[morning.length - 1] || null}, 
-          {startTime: afternoon.sort()[0] || null, endTime: afternoon.sort()[afternoon.length - 1] || null}, 
-          {startTime: night.sort()[0] || null, endTime: night.sort()[night.length - 1] || null}
-        ]
-      )
   }
+
+  return (
+      [
+        {startTime: morning.sort()[0] || null, endTime: morning.sort()[morning.length - 1] || null}, 
+        {startTime: afternoon.sort()[0] || null, endTime: afternoon.sort()[afternoon.length - 1] || null}, 
+        {startTime: night.sort()[0] || null, endTime: night.sort()[night.length - 1] || null}
+      ]
+    )
+}
