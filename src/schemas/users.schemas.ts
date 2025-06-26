54e2a59e1d9e4319dae535f064d72ff4a88b98dd
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-06-25 23:18:17 -0300

    version 1.12.106

--- src/schemas/users.schemas.ts
+++ src/schemas/users.schemas.ts
@@ -4,15 +4,7 @@ export const userTechnicalrSchema = z.object({
   name: z
     .string({ message: "Campo somente string" })
     .min(1, { message: "Campo obrigatório" })
-    .regex(/^[a-zA-Z\s]*$/, { message: "Campo nome deve conter apenas letras e espaços." })
-    .transform((name) => {
-      return name
-        .trim()
-        .split(" ")
-        .map((word) => {
-          return word[0].toUpperCase().concat(word.substring(1));
-        }).join(" ")
-    }),
+    .regex(/^[a-zA-Z\s]*$/, { message: "Campo nome deve conter apenas letras e espaços." }),
   email: z
     .string({ message: "Campo somente string" })
     .min(1, { message: "Campo obrigatório" })
@@ -23,7 +15,6 @@ export const userTechnicalrSchema = z.object({
 });
 
 export type UserTechnicalrSchema = z.infer<typeof userTechnicalrSchema>
-
 
 
 export const userCustomerSchema = z.object({
