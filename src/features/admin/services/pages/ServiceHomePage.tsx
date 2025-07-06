1c2b112e69e7dc28f64206ac19b4cba173360924
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-07-06 20:10:45 -0300

    version 1.16.20

--- src/features/admin/services/pages/ServiceHomePage.tsx
+++ src/features/admin/services/pages/ServiceHomePage.tsx
@@ -13,6 +13,7 @@ import { IconPlus } from "@/assets/icon/iconPlus"
 
 import { CreateModal } from "../components/createModal"
 import { UpdateModal } from "../components/updateModal"
+import { Pagination } from "@/components/pagination"
 
 export function Services(){
   const [modalNew, setModalNew] = useState(false)
@@ -49,40 +50,59 @@ export function Services(){
     
             <Table.Body>
 
-              <Table.Cell clas="lg:w-1/2 " internalSpacing="px-2 py-3 lg:px-4 lg:py-3">
-              <div className="max-sm:w-20 truncate ">
-                Instalação de Rede
-              </div>
-                
-              </Table.Cell>
+              {Array.from({ length: 10 }).map((_, i) => (
+                <>
+                  <tr className="border-t border-gray-500 text-left" key={i} >
+                    <Table.Cell clas="lg:w-1/2 " internalSpacing="px-2 py-3 lg:px-4 lg:py-3">
+                      <div className="max-sm:w-20 truncate ">
+                        Instalação de Rede
+                      </div>
+                        
+                      </Table.Cell>
 
-              <Table.Cell clas=" lg:w-full" internalSpacing="px-1 py-3 lg:px-4 lg:py-3">
-                <div className="max-sm:w-22">
-                  <span  >R$ 180,00</span>
-                </div>
-              </Table.Cell>
+                      <Table.Cell clas=" lg:w-full" internalSpacing="px-1 py-3 lg:px-4 lg:py-3">
+                        <div className="max-sm:w-22">
+                          <span  >R$ 180,00</span>
+                        </div>
+                      </Table.Cell>
 
-              <Table.Cell internalSpacing="py-3 lg:px-4 lg:py-3">
-                <div className="max-sm:hidden w-35"><Status type={status ? "active" : "inactive"} isButton={true} /></div>
-                <div className="lg:hidden flex justify-center"><Status type={status ? "active" : "inactive"} isButton={false} /></div>
-              </Table.Cell>
+                      <Table.Cell internalSpacing="py-3 lg:px-4 lg:py-3">
+                        <div className="max-sm:hidden w-35"><Status type={status ? "active" : "inactive"} isButton={true} /></div>
+                        <div className="lg:hidden flex justify-center"><Status type={status ? "active" : "inactive"} isButton={false} /></div>
+                      </Table.Cell>
 
-              <Table.Cell clas="flex justify-end" internalSpacing="px-2.5 py-3 lg:px-4 lg:py-3">
-                <div className="flex items-center gap-1 mr-2.5 max-sm:hidden">
-                  {status && <><IconBan className="w-4 h-4 cursor-pointer" onClick={() => setStatus(!status)} />Desativar</>}
-                  {!status && <><IconCicloCheck className="w-4 h-4 cursor-pointer" onClick={() => setStatus(!status)} />Reativar</>}
-                </div>
-                <div className="flex items-center gap-1 mr-2.5 lg:hidden">
-                  {status && <IconBan className="w-4 h-4 cursor-pointer" onClick={() => setStatus(!status)} />}
-                  {!status && <IconCicloCheck className="w-4 h-4 cursor-pointer" onClick={() => setStatus(!status)} />}
-                </div>
-                <UiButton type="button" typeColor="gray" typeSize="xxs" icon={IconPenLine} onClick={() => setModalEdition(!modalEdition)} />
-              </Table.Cell>
+                      <Table.Cell clas="flex justify-end" internalSpacing="px-2.5 py-3 lg:px-4 lg:py-3">
+                        <div className="flex items-center gap-1 mr-2.5 max-sm:hidden">
+                          {status && <><IconBan className="w-4 h-4 cursor-pointer" onClick={() => setStatus(!status)} />Desativar</>}
+                          {!status && <><IconCicloCheck className="w-4 h-4 cursor-pointer" onClick={() => setStatus(!status)} />Reativar</>}
+                        </div>
+                        <div className="flex items-center gap-1 mr-2.5 lg:hidden">
+                          {status && <IconBan className="w-4 h-4 cursor-pointer" onClick={() => setStatus(!status)} />}
+                          {!status && <IconCicloCheck className="w-4 h-4 cursor-pointer" onClick={() => setStatus(!status)} />}
+                        </div>
+                        <UiButton type="button" typeColor="gray" typeSize="xxs" icon={IconPenLine} onClick={() => setModalEdition(!modalEdition)} />
+                      </Table.Cell>
+                    </tr>
+                </>
+               ))
+              }
 
             </Table.Body>
           </Table.Root>
         </div>
       
+      {/* PAGINAÇÃO */}
+      {/* <Pagination.Root>
+        <Pagination.Previous previous={pagination?.previous} onClick={() => setPage(page - 1)} />
+          <Pagination.Body 
+            pagination={pagination} 
+            onClickPrevius={() => setPage(pagination?.previous as number)} 
+            onClickNext={() => setPage(pagination?.next as number)}
+            page={page}
+          />
+          <Pagination.Next next={pagination?.next} onClick={() => setPage(page + 1)} />
+      </Pagination.Root> */}
+      {/* PAGINAÇÃO */}
     </>
   )
 }
