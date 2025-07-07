e583f372bef95b89f22353bdf1028fb0213b1ab7
Ricardo Tavares <ricardo.ti.sistemas@gmail.com>
2025-07-07 18:23:53 -0300

    version 1.16.23 - currency correction => upa sa

--- src/lib/currency.ts
+++ src/lib/currency.ts
@@ -2,8 +2,8 @@ export function currency(value: string ): string{
   // Remove tudo que não for número
   const numeric = value.replace(/\D/g, "")
 
-   // Divide por 100 para ter centavos
-  const iscurrency = Number(numeric) / 100
+   // Divide por 100 para ter centavos Number(numeric) / 100
+  const iscurrency = Number(numeric)
 
   // Formata para BRL
   const convert = iscurrency.toLocaleString("pt-BR", {
