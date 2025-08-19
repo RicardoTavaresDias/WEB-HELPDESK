28c6c84c86afe44255d946e86bb72470fc5c0298
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-08-19 00:35:19 -0300

    version 1.21.23

--- src/features/layout/http/use-get-whatsapp.ts
+++ src/features/layout/http/use-get-whatsapp.ts
@@ -1,10 +1,17 @@
 import { urlWhatsapp } from "@/services/api"
 import { useMutation } from "@tanstack/react-query"
 
+const token = localStorage.getItem("helpDesk:token")
+
 function useGetWhatsapp () {
   return useMutation({
     mutationFn: async () => {
-      const response = await fetch(urlWhatsapp)
+      const response = await fetch(urlWhatsapp, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
       const result = await response.json()
       return result.message
     }
